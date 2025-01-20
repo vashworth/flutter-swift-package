167e70a4e66353b6bace11cf00f7f914268d2de4
Victoria Ashworth <vashworth@google.com>
2025-01-20 16:17:13 -0600

    Add flutter dependency

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FlutterSwiftPackage"),
+            name: "FlutterSwiftPackage", dependencies: [.target(name: "Flutter")]),
 //        .testTarget(
 //            name: "flutter_swift_packageTests",
 //            dependencies: ["flutter_swift_package"]
